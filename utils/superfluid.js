2d42dd6a90e4d247bb944f4e2f691734922e1d17
Pawan <pawanbajaj2012@gmail.com>
2023-04-15 16:05:26 +0900

    superfluid stream

--- utils/superfluid.js
+++ utils/superfluid.js
@@ -6,6 +6,8 @@ export const createFlow = async (sender, receiver, flowRate, providerOrSigner) =
   // const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
   // const signer = await provider.getSigner()
   // const sender = await signer.getAddress()
+
+  
   const sf = await Framework.create({
     chainId: 80001,
     provider
@@ -27,12 +29,15 @@ export const createFlow = async (sender, receiver, flowRate, providerOrSigner) =
 
 export const getFlow = async (sender, receiver, providerOrSigner) => {
   // const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  // const signer = await provider.getSigner()
+  // const sender = await signer.getAddress()
+
+
   const sf = await Framework.create({
     chainId: 80001,
     provider
   });
-  const signer = await provider.getSigner()
-  const sender = await signer.getAddress()
+  
   const daix = await sf.loadSuperToken("fDAIx");
 
   let res = await daix.getFlow({
